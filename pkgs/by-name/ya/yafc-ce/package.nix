9f5e743e3efc933d0e7e385ec2c83e173afe860c
TheColorman <github@colorman.me>
2024-10-27 15:38:33 +1100

    yafc-ce: init at 2.0.1
    Advances Factorio calculator with mod support. Community Edition of
    YAFC.
    
    https://github.com/shpaass/yafc-ce

--- pkgs/by-name/ya/yafc-ce/package.nix
+++ pkgs/by-name/ya/yafc-ce/package.nix
@@ -1,7 +1,9 @@
 {
+  buildDotnetModule,
+  stdenv,
+  lua,
   lib,
   fetchFromGitHub,
-  buildDotnetModule,
   dotnetCorePackages,
   SDL2,
   SDL2_image,
@@ -11,42 +13,55 @@
 let
   dotnet = dotnetCorePackages.dotnet_8;
   pname = "yafc-ce";
-  version = "0.9.1";
+  version = "2.0.1";
 
   src = fetchFromGitHub {
     owner = "shpaass";
     repo = "yafc-ce";
     rev = version;
-    hash = "sha256-M3oBHFTLU3DTbxgwtccZq8L92A0kWb9bJs5RrOUQidY=";
+    hash = "sha256-qJOEdP2jRqK0E9Q4n7YgO5jyHxztb8aVOTlLDq1VvMk=";
   };
 in
 buildDotnetModule {
   inherit pname version src;
 
-  projectFile = "Yafc/Yafc.csproj";
+  projectFile = [ "Yafc/Yafc.csproj" ];
+  testProjectFile = [ "Yafc.Model.Tests/Yafc.Model.Tests.csproj" ];
   nugetDeps = ./deps.nix;
+
   dotnet-sdk = dotnet.sdk;
   dotnet-runtime = dotnet.runtime;
+
   executables = [ "Yafc" ];
 
   runtimeDeps = [
     SDL2
-    SDL2_image
     SDL2_ttf
+    SDL2_image
   ];
 
-  meta = {
-    homepage = "https://github.com/shpaass/yafc-ce";
-    description = "Planner and analyzer for Factorio";
+  # Arm requires a direct reference to liblua.so.5.2
+  postInstall = lib.strings.optionalString stdenv.hostPlatform.isAarch64 ''
+    install -Dm644 "${lua}/lib/liblua.so.5.2" "$out/lib/yafc-ce/liblua52.so"
+  '';
+
+  meta = rec {
+    description = "Powerful Factorio calculator/analyser that works with mods, Community Edition";
     longDescription = ''
       Yet Another Factorio Calculator or YAFC is a planner and analyzer.
       The main goal of YAFC is to help with heavily modded Factorio games.
 
       YAFC Community Edition is an updated and actively-maintained version of the original YAFC.
     '';
-    platforms = lib.platforms.x86_64;
+    homepage = "https://github.com/shpaass/yafc-ce";
+    downloadPage = "https://github.com/shpaass/yafc-ce/releases/tag/${version}";
+    changelog = downloadPage;
     license = lib.licenses.gpl3;
-    maintainers = with lib.maintainers; [ diamond-deluxe ];
+    maintainers = with lib.maintainers; [
+      diamond-deluxe
+      TheColorman
+    ];
+    platforms = with lib.platforms; x86_64 ++ darwin;
     mainProgram = "Yafc";
   };
 }
